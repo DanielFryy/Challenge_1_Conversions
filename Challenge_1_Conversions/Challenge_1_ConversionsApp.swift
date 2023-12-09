c2bcffb835bab035a609c20c906ce33a24ee5234
Daniel Freire <dgz1606@gmail.com>
2023-12-09 18:17:26 -0500

    Initial Commit

---
+++ Challenge_1_Conversions/Challenge_1_ConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge_1_ConversionsApp.swift
+//  Challenge_1_Conversions
+//
+//  Created by Daniel Freire on 12/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Challenge_1_ConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
